911658b46406a2b305bcc98fc7663e3da4df1197
myvictorlife <victorcmggg@gmail.com>
2023-09-15 12:56:31 +0200

    fix: partial

--- apps/exercises/src/app/app.config.ts
+++ apps/exercises/src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
+  withComponentInputBinding,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes, withComponentInputBinding(), withEnabledBlockingInitialNavigation()),
+  ],
 };
--- apps/exercises/src/app/app.routes.ts
+++ apps/exercises/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Route } from '@angular/router';
 import { JsConcurrencyExerciseComponent } from '../pages/exercises-page/js-concurrency-exercise/js-concurrency-exercise.component';
+import { WelcomeComponent } from '../pages/welcome/welcome.component';
 
 export const appRoutes: Route[] = [
+    // {
+    //     path: '',
+    //     component: WelcomeComponent
+    // },
     {
         path: 'js-concurrency-exercise',
         component: JsConcurrencyExerciseComponent
