d1d1da8a952311cbf407e88529425d6d373c945f
myvictorlife <victorcmggg@gmail.com>
2023-09-17 17:54:02 +0200

    fix: remove unused files

--- apps/exercises/src/app/app.routes.ts
+++ apps/exercises/src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Route } from '@angular/router';
-import { JsConcurrencyExercisePage } from '../pages/exercises-page/js-concurrency-exercise/js-concurrency-exercise.page';
 
-import { WelcomeComponent } from '../pages/welcome/welcome.component';
-
-export const appRoutes: Route[] = [
-    // {
-    //     path: '',
-    //     component: WelcomeComponent
-    // },
-    {
-        path: 'js-concurrency-exercise',
-        component: JsConcurrencyExercisePage
-    }
-];
+export const appRoutes: Route[] = [];
